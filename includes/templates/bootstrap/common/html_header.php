168b1273d9367abea4dcc6fafad85a4855594fa6
Scott C Wilson <scottcwilson@gmail.com>
2021-07-01 14:00:02 -0400

    Add client provided stylesheet functionality

--- includes/templates/bootstrap/common/html_header.php
+++ includes/templates/bootstrap/common/html_header.php
@@ -138,6 +138,12 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 
     require($template->get_template_dir('stylesheet_zca_colors.php', DIR_WS_TEMPLATE, $current_page_base, 'css') . '/stylesheet_zca_colors.php');
 
+    // User defined styles come last 
+    $client_styles = DIR_WS_TEMPLATE . 'css/client_styles.php'; 
+    if (file_exists($client_styles)) {
+       include($client_styles); 
+    }
+
     /** CDN for jQuery core * */
     ?>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
